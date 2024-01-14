521a58dbd9ac4f2c1d94fc370910a79408e14a9c
nitingaurr <ng8901087249@gmail.com>
2024-01-14 22:49:05 +0530

    trying to create logic for users sending messages to one another almost there

--- backend/files/index.ts
+++ backend/files/index.ts
@@ -4,29 +4,46 @@ import { Express } from "express";
 
 const wss = new WebSocketServer({port:8080})
 
-const store = []
-const clients = new Map();
+type C = {
+    clientId:string,
+    ws:WebSocketServer
+}
 
+
+
+
+const clientsID:string[]= []
+
+const Instance:{[clientId:string]:{ws:any}} ={}
 
 wss.on("connection",(ws , req) => {
   
     console.log('running')
     
-    const clientId = Math.round(Math.random()*1000)
+    const clientId = String(Math.round(Math.random()*1000))
   
     console.log("client id for "+ clientId)
-     clients.set(clientId,ws)
-    const key = req.headers['sec-websocket-key']
-    console.log("key value for clients"+key)
+     clientsID.push( clientId)
+     Instance[clientId] = {ws}
+     ws.send(JSON.stringify({ type: 'clientId', content: clientId }));
     ws.on('message',(message) => {
-        ws.send(JSON.stringify({ type: 'clientId', content: clientId }));
+        
         console.log("client"+ message)
-    
+        console.log("message after parsing"+ JSON.parse(message.toString()))
+        const { cid, content} = JSON.parse(message.toString()) 
+        console.log('value of client id with message'+cid , content)
+        const otherClientId = clientsID.filter((id) => id !== cid
+        )[0]
+        console.log("value of array stored all client id"+clientsID)
+        console.log("value of other clients id getting using filter"+otherClientId)
+    }
       
-    })
+    )
 
     ws.on('close', () => {
         console.log('Client disconnected');
       });
+
+      
 })
 
